ae5be57a9e7b14cf7ac9ea89009a169ada8d95c6
ItisCaleb <40360126+ItisCaleb@users.noreply.github.com>
2020-03-10 17:02:42 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,14 @@ const info =require('../public/info');
 
 router.get("/",function (req,res) {
     res.render('index',{
-        title:info.title[0],
-        content:info.news
+        title:"首頁",
+        content:"施工中"
     });
 });
 router.get("/about",function (req,res) {
     res.render('index',{
-        title:info.title[1],
-        content:info.info
+        title:"關於",
+        content:"這裡要放介紹"
     });
 });
 router.get("/register",function (req,res) {
