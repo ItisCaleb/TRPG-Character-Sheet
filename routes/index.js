66a5e3d0709a65a32c858ecc73c2f299312454cb
ItisCaleb <40360126+ItisCaleb@users.noreply.github.com>
2020-03-10 15:00:35 +0800

    add info.json

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 const router = require('express').Router();
+const info =require('../info');
 
 router.get("/",function (req,res) {
     res.render('index',{
-        title:"首頁"
+        title:info.title[0],
+        content:info.news
     });
 });
 router.get("/about",function (req,res) {
     res.render('index',{
-        title:"關於"
+        title:info.title[1],
+        content:info.info
     });
 });
 router.get("/register",function (req,res) {
