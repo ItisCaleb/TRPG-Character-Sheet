ea5ff48a14ba8f47922de689f6e080a43ac565c4
SilkooDarze <tsaotsaotsao51510@gmail.com>
2020-03-15 18:12:41 +0800

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 
-//routemiddleware
+//route middle ware
 app.use("/api/user", authRoute);
 app.use('/', indexRoute);
 
