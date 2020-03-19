c0b7b24ab27846e8a9c49a710fbf3c55818b0528
SilkooDarze <tsaotsaotsao51510@gmail.com>
2020-03-19 22:29:28 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,53 +4,53 @@ const jwtDecode = require('jwt-decode');
 const verify = require('../public/js/verifyToken');
 
 //render main page
-router.get("/",function (req,res) {
-    res.render('index',{
-        title:info.title[0],
-        content:info.news
+router.get("/", function (req, res) {
+    res.render('index', {
+        title: info.title[0],
+        content: info.news
     });
 });
 //render create page and check if the user is already login
-router.get("/create",verify,function (req,res) {
-    res.render('index',{
-        title:info.title[1],
-        content:info.create
+router.get("/create", verify, function (req, res) {
+    res.render('index', {
+        title: info.title[1],
+        content: info.create
     });
 });
 //render about page
-router.get("/about",function (req,res) {
-    res.render('index',{
-        title:info.title[2],
-        content:info.info
+router.get("/about", function (req, res) {
+    res.render('index', {
+        title: info.title[2],
+        content: info.info
     });
 });
 //render sign up page
-router.get("/signup",function (req,res) {
+router.get("/signup", function (req, res) {
     res.render('register');
 });
 //render login page
-router.get("/login",function (req,res) {
+router.get("/login", function (req, res) {
     res.render('login');
 });
 //render user page and check if the user is already login
-router.get("/user",verify,function (req,res) {
+router.get("/user", verify, function (req, res) {
     const userinfo = jwtDecode(req.cookies.auth_token);
-    res.render('user',{
-        title:userinfo.name + info.title[3],
-        email:userinfo.email
+    res.render('user', {
+        title: userinfo.name + info.title[3],
+        email: userinfo.email
     });
 });
-router.get('/adminpost',verify,function (req,res) {
-    if(jwtDecode(req.cookies.auth_token).name===process.env.ADMIN){
-            res.render('admin');
-        }else{
-            res.cookie('ValidValue','非管理員憑證');
-            res.redirect('/');
-        }
+router.get('/adminpost', verify, function (req, res) {
+    if (jwtDecode(req.cookies.auth_token).name === process.env.ADMIN) {
+        res.render('admin');
+    } else {
+        res.cookie('ValidValue', '非管理員憑證');
+        res.redirect('/');
+    }
 });
-router.get('/create-session',verify,function (req,res) {
+router.get('/create-session', verify, function (req, res) {
     res.render('trpg_session');
 });
 
 
-module.exports=router;
+module.exports = router;
