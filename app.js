1d91c07e906f4723fa197cf26b8d33efdabcd574
ItisCaleb <40360126+ItisCaleb@users.noreply.github.com>
2020-03-21 01:28:01 +0800

    Update app.js

--- app.js
+++ app.js
@@ -58,9 +58,6 @@ app.use(function (err, req, res, next) {
 
 io.on('connection',function (socket) {
     console.log('a user connected');
-    socket.on('disconnect',function () {
-        console.log('user disconnected');
-    })
 });
 
 // start server
