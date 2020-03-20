95ddcc88c2209f4b9880352aac3ee1752e525b85
ItisCaleb <40360126+ItisCaleb@users.noreply.github.com>
2020-03-21 01:32:40 +0800

    Update findSession.js

--- public/js/findSession.js
+++ public/js/findSession.js
@@ -6,12 +6,10 @@ module.exports = async function (req,res,next) {
     const gm_name = jwtDecode(req.cookies.auth_token).name;
     const gm =  await Session.find({gm: gm_name});
     if (!gm) {
-        console.log(gm);
+        console.log('你還沒創建任何團務');
         next();
  }else{
         console.log(gm);
         next();
     }
-   //else if(gm) return await Session.find({name:gm_name});
-
 };
