4044eddb02b2c7f9140d902d827ab50ec550be41
ItisCaleb <40360126+ItisCaleb@users.noreply.github.com>
2020-03-30 20:35:52 +0800

    Update socket.js

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,7 +1,7 @@
 document.write('');
 $(document).ready(function () {
     //connect socket to the server
-    const socket = io('localhost:3000');
+    const socket = io('kulimi.cnmc.tw:3000');
     socket.on('alert',function (data) {
         alert(data);
     });
