c12b0bbd84ee8f3bf8adeb2ad3a1912366255bf8
ItisCaleb <40360126+ItisCaleb@users.noreply.github.com>
2020-03-30 20:33:37 +0800

    Update TRPGSession.js

--- routes/TRPGSession.js
+++ routes/TRPGSession.js
@@ -8,11 +8,11 @@ const {sessionValidation} = require("../public/js/validation");
 router.post('/TRPGCreateSession',async function (req,res) {
     //check if the format is correct
     const {error}= sessionValidation(req.body);
-    if(error) return res.status(400).redirect('/createsession'),req.app.io.emit('alert',error.details[0].message);
+    if(error) return res.status(400).redirect('/trpgsession/create'),req.app.io.emit('alert',error.details[0].message);
 
     //check if the session is already exist
     const sessionExist = await Session.findOne({name:req.body.name});
-    if (sessionExist) return res.status(400).redirect('/createsession'),req.app.io.emit('alert','此名稱已存在');
+    if (sessionExist) return res.status(400).redirect('/trpgsession/create'),req.app.io.emit('alert','此名稱已存在');
 
     //decode auth_token to get user information
     const user = jwtDecode(req.cookies.auth_token).name;
@@ -38,7 +38,7 @@ router.post('/TRPGCreateSession',async function (req,res) {
 router.post('/TRPGJoinSession',async function (req,res) {
     //check if the format is correct
     const {error}= sessionValidation(req.body);
-    if(error) return res.status(400).redirect('/joinsession'),req.app.io.emit('alert',error.details[0].message);
+    if(error) return res.status(400).redirect('/trpgsession/join'),req.app.io.emit('alert',error.details[0].message);
 
     //decode
     const user = jwtDecode(req.cookies.auth_token).name;
@@ -47,11 +47,11 @@ router.post('/TRPGJoinSession',async function (req,res) {
     //check if the player is already in the session
     const playerExist = await Session.findOne({name:req.body.name,player:{$in:[user]}});
     //check if the session doesn't exist
-    if (!session) return res.status(400).redirect('/joinsession'),req.app.io.emit('alert','此團務不存在');
+    if (!session) return res.status(400).redirect('/trpgsession/join'),req.app.io.emit('alert','此團務不存在');
     //check the password
     if (req.body.password !== session.password ) return res.status(400).redirect('/joinsession'),req.app.io.emit('alert', '密碼錯誤');
 
-    if (playerExist) return res.status(400).redirect('/joinsession'),req.app.io.emit('alert','你已加入此團務');
+    if (playerExist) return res.status(400).redirect('/trpgsession/join'),req.app.io.emit('alert','你已加入此團務');
     try{
         await Session.update({name:req.body.name},{$addToSet:{player:user}});
         req.app.io.emit('alert',req.body.name + '加入成功'+' GM:'+session.gm);
@@ -65,7 +65,7 @@ router.post('/TRPGJoinSession',async function (req,res) {
 router.get('/delete/:id',async function (req,res) {
     const user=jwtDecode(req.cookies.auth_token).name;
     const session = await Session.findOne({_id:req.params.id});
-    if(session.gm === jwtDecode(user)){
+    if(session.gm === user){
         req.app.io.emit('alert',session.name+'已被刪除');
         await Session.deleteOne({_id:req.params.id});
         res.redirect('/trpgsession');
