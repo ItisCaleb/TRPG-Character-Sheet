4987ab34ef76a6e5174c9f740d56514120abd2f8
ItisCaleb <happycaleb1212@gmail.com>
2020-04-24 12:39:51 +0800

    test update

--- app.js
+++ app.js
@@ -71,6 +71,7 @@ const httpRouter = express.Router();
 httpApp.use('/', httpRouter);
 httpRouter.get('/', function(req, res){
     var host = req.get('Host');
+    console.log(host);
     // replace the port in the host
     host = host.replace(/:\d+$/, ":"+port);
 
