6040d4824a6ce3929054252b3d8b2de63d5a6cac
ItisCaleb <40360126+ItisCaleb@users.noreply.github.com>
2020-06-03 16:12:00 +0800

    typesetting

--- public/js/DND5e.js
+++ public/js/DND5e.js
@@ -2,6 +2,7 @@ document.write();
 $(document).ready(function () {
     sheetSetup()
     optionSetup()
+    $('textarea[name=equip]').css('height',$('textarea[name=equip]').parent().css('height'));
     $(document).on('input','input',function () {
         sheetSetup()
     })
@@ -15,13 +16,17 @@ $(document).ready(function () {
 
     $(document).on("click", ".create", function (e){
       e.preventDefault();
-      const calSheet =sheetPush()
+      const calSheet =sheetPush();
+      const spell =calSheet.spell;
       const skill =calSheet.skill;
       const sheet = $('#myform').serializeArray();
       sheet.push(skill);
+      sheet.push(spell);
       console.log(sheet);
     })
+
     function sheetSetup() {
+
         $('input[type=number]').each(function () {
             if($(this).val()<0 || $(this).val()===String ||$(this).val()===""){
                 $(this).val(0);
@@ -63,12 +68,27 @@ $(document).ready(function () {
     }
     function sheetPush() {
         var skill=[];
+        var spell={0:{},1:{},2:{},3:{},4:{},5:{},6:{},7:{},8:{},9:{}};
         $('.skill').each(function () {
             const input = $(this).siblings('label');
             if($(this).siblings('label').find('input').val()==='yes'){
                 skill.push(input.text())
             }
         })
+
+        $('.spell-level').each(function (index) {
+            spell[index]["number"]=[];
+            spell[index]["spells"]={};
+            spell[index]["number"].push($(this).siblings('label').children('.spell-number').val());
+            spell[index]["number"].push($(this).siblings('label').children('.spell-number').val());
+            $(this).parent().siblings().children('label').each(function (i) {
+                if ($(this).children('.spell-name').val()!==("")){
+                    const name=$(this).children('.spell-name').val();
+                    spell[index]["spells"][name]=($(this).children('.check-input').val());
+                }
+            })
+        })
+        this.spell=spell;
         this.skill=skill;
         return this;
     }
