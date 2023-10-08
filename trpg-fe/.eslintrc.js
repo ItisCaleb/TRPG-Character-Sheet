1c9f22715de3bef9f11fc2c779dedee3080ab020
ItisCaleb <happycaleb1212@gmail.com>
2023-10-08 22:34:06 +0800

    Fix dependency

--- trpg-fe/.eslintrc.js
+++ trpg-fe/.eslintrc.js
@@ -8,11 +8,16 @@ module.exports = {
     'eslint:recommended'
   ],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'vue/no-parsing-error': [2, { "x-invalid-end-tag": false }]
+    'vue/no-parsing-error': [2, { "x-invalid-end-tag": false }],
+    'vue/multi-word-component-names': 'off',
+    'vue/no-reserved-component-names': 'off',
+    'vue/no-mutating-props': 'off'
+    
   }
 }
